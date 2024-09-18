9bc32f6bbfedfa97b40b6c0bec9dcb6430f9462d
sanchezjuliomarcelo <sanchezjuliomarcelo@gmail.com>
2024-09-18 19:39:42 -0300

    url productivas

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $.getScript('functions.js', function() {
+$(document).ready(function () {
+    $.getScript('functions.js', function () {
         $('#btnBuscar').click(buscarProductos);
         $('#btnBorrar').click(() => {
             borrarProductos();
@@ -13,7 +13,7 @@ $(document).ready(function() {
                 url,
                 method: 'GET',
                 dataType: 'xml',
-                success: function(data) {
+                success: function (data) {
                     if (clearCards) {
                         $('#productList').empty();
                         productos = [];
@@ -38,7 +38,7 @@ $(document).ready(function() {
                     $('#mayorDescuentoItemsCount').text(`Mayor Descuento Items: ${totalMayorDescuentoItems}`);
 
                     function processItems(items, list, category) {
-                        items.each(function() {
+                        items.each(function () {
                             const producto = {
                                 productId: $(this).find('product_id').text() || '',
                                 gId: $(this).find('g\\:id').text() || $(this).find('id').text() || '',
@@ -53,30 +53,35 @@ $(document).ready(function() {
                                 link: $(this).find('g\\:link').text() || $(this).find('link').text() || '',
                                 imageLink: $(this).find('g\\:image_link').text() || $(this).find('image_link').text() || '',
                                 availability: $(this).find('g\\:availability').text() || $(this).find('availability').text() || '',
-                                price: $(this).find('g\\:price').text() || $(this).find('price').text() || '', // Procesar el precio tachado
+                                price: $(this).find('g\\:price').text() || $(this).find('price').text() || '',
                                 salePrice: $(this).find('g\\:sale_price').text() || $(this).find('sale_price').text() || '',
                                 installmentAmount: $(this).find('g\\:installment').find('g\\:amount').text() || $(this).find('amount').text() || '',
                                 installmentMonths: $(this).find('g\\:installment').find('g\\:months').text() || $(this).find('months').text() || '',
-                                customLabel1: $(this).find('g\\:custom_label_1').text() || $(this).find('custom_label_1').text() || '',
+                                customLabel1: $(this).find('g\\:custom_label_1').html() || $(this).find('custom_label_1').text() || '',
                                 customLabel2: $(this).find('g\\:custom_label_2').text() || $(this).find('custom_label_2').text() || '',
                                 customLabel3: $(this).find('g\\:custom_label_3').text() || $(this).find('custom_label_3').text() || '',
                                 customLabel4: $(this).find('g\\:custom_label_4').text() || $(this).find('custom_label_4').text() || '',
                                 category: category
                             };
-                    
+
                             // Procesar description_attributes para mostrar como texto plano
-                            $(this).find('description_attributes').each(function() {
+                            $(this).find('description_attributes').each(function () {
                                 producto.descriptionAttributes += $(this).text() + '<br>';
                             });
-                    
+
                             productos.push(producto);
-                    
+
+                            // Limpiar caracteres adicionales de precio
+                            const cleanPrice = producto.price.replace(/]]>/g, '');
+
+                            // Limpiar etiquetas CDATA de custom_label_1
+                            const cleanCustomLabel1 = producto.customLabel1
+                                .replace('<![CDATA[', '') // Elimina apertura de CDATA
+                                .replace(']]>', '');       // Elimina cierre de CDATA
+
                             const card = `
                                 <div class="col-md-3 product-card">
                                     <div class="card mb-3">
-                                        <div class="card-header" style="background-color: #f8f9fa;">
-                                            <strong>${producto.category}</strong>
-                                        </div>
                                         <img src="${producto.imageLink}" class="card-img-top" alt="${producto.title}">
                                         <div class="card-body">
                                             <h5 class="card-title">${producto.title}</h5>
@@ -88,11 +93,11 @@ $(document).ready(function() {
                                             <p class="card-text"><strong>GTIN:</strong> ${producto.gtin}</p>
                                             <p class="card-text"><strong>Condición:</strong> ${producto.condition}</p>
                                             <p class="card-text"><strong>Disponibilidad:</strong> ${producto.availability}</p>
-                                            <p class="card-text"><strong>Precio:</strong> ${producto.price}</p> <!-- Mostrar el precio con tachado -->
+                                            <p class="card-text"><strong>Precio:</strong> <span style="text-decoration: line-through;">${cleanPrice}</span></p>
                                             <p class="card-text"><strong>Precio de venta:</strong> ${producto.salePrice}</p>
                                             <p class="card-text"><strong>Monto:</strong> ${producto.installmentAmount}</p>
                                             <p class="card-text"><strong>Meses:</strong> ${producto.installmentMonths}</p>
-                                            <p class="card-text"><strong>Descuento Estandar:</strong> ${producto.customLabel1}</p>
+                                            <p class="card-text"><strong>Descuento Estandar:</strong> <span>${cleanCustomLabel1}</span></p>
                                             <p class="card-text"><strong>PVP en 1 Pago:</strong> ${producto.customLabel2}</p>
                                             <p class="card-text"><strong>% Descuento en 1 Pago:</strong> ${producto.customLabel3}</p>
                                             <p class="card-text"><strong>$ Descuento en 1 Pago:</strong> ${producto.customLabel4}</p>
@@ -101,11 +106,11 @@ $(document).ready(function() {
                                     </div>
                                 </div>
                             `;
-                    
+
                             list.append(card);
                         });
                     }
-                    
+
 
                     // Procesar y mostrar los productos
                     processItems(todosLosProductos, $('#productList'), 'Todos los Productos');
@@ -120,7 +125,7 @@ $(document).ready(function() {
                     // Finalizar la carga
                     ocultarIndicadorCarga();
                 },
-                error: function() {
+                error: function () {
                     alert('Error al cargar los datos.');
                     ocultarIndicadorCarga();
                 }
@@ -149,7 +154,7 @@ $(document).ready(function() {
                 alert('No hay productos para exportar.');
                 return;
             }
-        
+
             // Mapear los datos de los productos a un formato adecuado para Excel
             const data = productos.map(producto => {
                 return {
@@ -177,14 +182,14 @@ $(document).ready(function() {
                     'Categoría': producto.category || '' // Asegúrate de que `category` esté disponible
                 };
             });
-        
+
             // Convertir los datos a una hoja de trabajo
             const ws = XLSX.utils.json_to_sheet(data);
-            
+
             // Crear un libro de trabajo
             const wb = XLSX.utils.book_new();
             XLSX.utils.book_append_sheet(wb, ws, "Productos");
-        
+
             // Exportar el libro de trabajo como archivo Excel
             XLSX.writeFile(wb, "productos.xlsx");
         }
